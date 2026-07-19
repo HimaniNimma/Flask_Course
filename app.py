c674bff90ba1de47c92fddec6271d53926fab7de
HimaniNimma <it1298himani@gmail.com>
2026-07-19 21:32:13 +0530

    Implement template inheritance and navigation pages

--- app.py
+++ app.py
@@ -2,8 +2,23 @@ from flask import Flask, render_template
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("index.html")
+    name = "Himani"
+    course="Flask"
+    age=21
+    is_student=True
+    skills = ["Python", "Flask", "HTML", "CSS"]
+    return render_template(
+        "index.html",
+        name=name,
+        age=age,
+        course=course,
+        is_student=is_student,
+        skills=skills
+    )
 @app.route("/about")
 def about():
-    return "This is the about page of my Flask application."
+    return render_template("about.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 app.run(debug=True)
